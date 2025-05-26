09d54579fce0031bdcd84e8d5ab18849982a4995
Gertinjo <gerticalaj2013@gmail.com>
2025-05-26 19:20:43 +0200

    pprpa

---
+++ fread.php
@@ -0,0 +1,25 @@
+<?php
+
+$myfile = fopen("ds.text");
+$text = fopen("$myfile" , 'r');
+$my_file_read= filesize($text);
+echo $myfile ,  $my_file_read;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// $file_name = "ds.txt";
+// $print = fopen($file_name , "w");
+
+
+$myfile = fopen("ds.text" , "w");
+$text = "Digital School";
+fwrite($myfile , $text);
+
+
+
+
+
+
+?>
